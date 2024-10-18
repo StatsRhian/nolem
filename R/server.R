6e53b597fc324f792598997cf871eca47952c6b9
Rhian Davies <thetrianglegirl@gmail.com>
2024-10-18 17:05:49 +0100

    Pass R CMD

--- R/server.R
+++ R/server.R
@@ -5,11 +5,11 @@ app_server = function(input, output, session) {
 
     output$distPlot <- shiny::renderPlot({
 
-        x  <- faithful[, 2]
+        x  <- datasets::faithful[, 2]
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+        graphics::hist(x, breaks = bins, col = 'darkgray', border = 'white',
              xlab = 'Waiting time to next eruption (in mins)',
              main = 'Histogram of waiting times')
 
